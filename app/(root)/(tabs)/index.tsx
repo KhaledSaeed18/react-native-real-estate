300575b75da243aef32c70b6842d261a976f8d3e
KhaledSaeed18 <khaled18saeed@gmail.com>
2024-12-22 11:50:12 +0200

    feat: add tab navigation layout and update index screen; include new icons and styles

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,19 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold font-rubik text-3xl my-10">Welcome to ReState</Text>
-      <Link href="/sign-in">
-        Sign In
-      </Link>
-      <Link href="/explore">
-        Explore
-      </Link>
-      <Link href="/profile">
-        Profile
-      </Link>
-      <Link href="/properties/1">
-        Property 1
-      </Link>
+      <Text className="font-bold font-rubik text-3xl my-10">
+        Welcome to ReState
+      </Text>
     </View>
   );
 }
